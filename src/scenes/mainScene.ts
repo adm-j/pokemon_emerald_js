b874bcc67b800c58feea187b3271f056b9049075
Adam <ad-91@live.co.uk>
2024-03-28 18:20:06 +0000

    Updated code to update scene name and variables to littleroottown

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -3,7 +3,7 @@ import Vector2 = Phaser.Math.Vector2;
 import {player} from "../player/player.ts";
 
 import internalTiles from "../assets/tileset/emerald_tileset.png"
-import newBarkTown from "../assets/tilemap/newbarktown.json";
+import littleRootTown from "../assets/tilemap/littleroottown.json";
 import playerDown from "../assets/sprites/male_pc_down.png";
 import playerUp from "../assets/sprites/male_pc_up.png";
 import playerRight from "../assets/sprites/male_pc_right.png";
@@ -19,7 +19,7 @@ export class mainScene extends Phaser.Scene {
 
     preload() {
         this.load.image("internal_tiles", internalTiles);
-        this.load.tilemapTiledJSON("new_bark_town", newBarkTown);
+        this.load.tilemapTiledJSON("little_root_town", littleRootTown);
         this.load.spritesheet("player_up", playerUp, {frameWidth: 15, frameHeight: 22, spacing: 1});
         this.load.spritesheet("player_down", playerDown, {frameWidth: 15, frameHeight: 22, spacing: 1});
         this.load.spritesheet("player_right", playerRight, {frameWidth: 15, frameHeight: 22, spacing: 1});
@@ -27,12 +27,15 @@ export class mainScene extends Phaser.Scene {
     }
 
     create() {
-        const map = this.make.tilemap({key: "new_bark_town"});
+        const map = this.make.tilemap({key: "little_root_town"});
         const tileset = map.addTilesetImage("emerald_exterior", "internal_tiles")!;
         const ground = map.createLayer("ground", tileset, 0, 0);
         const trees = map.createLayer("trees", tileset, 0, 0);
         const buildings = map.createLayer("buildings", tileset, 0, 0);
         const objects = map.createLayer("objects", tileset, 0, 0);
+
+        buildings?.setDepth(1);
+        trees?.setDepth(1);
 
         // this.sprite = this.add.sprite(0,0, "player_down", 0);
         const pcSprite = this.add.sprite(0,0, "player_down", 0)
